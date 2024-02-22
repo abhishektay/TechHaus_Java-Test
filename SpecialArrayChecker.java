7278168f4783961ea2433cac6e7fa6a35440801a
Abhishek Tayal <91242899+abhishektay@users.noreply.github.com>
2024-02-22 15:53:11 +0530

    Update SpecialArrayChecker.java

--- SpecialArrayChecker.java
+++ SpecialArrayChecker.java
@@ -4,18 +4,17 @@ public class SpecialArrayChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Initialize a 3x3 array
+
         int[][] array = new int[3][3];
         
-        // Input values into the array
+      
         System.out.println("Enter values into the 3x3 array:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 array[i][j] = scanner.nextInt();
             }
         }
-        
-        // Calculate sum of even and odd elements
+       
         int sumEven = 0;
         int sumOdd = 0;
         for (int[] row : array) {
@@ -28,7 +27,7 @@ public class SpecialArrayChecker {
             }
         }
         
-        // Check if the array is special
+ 
         if (sumEven == sumOdd) {
             System.out.println("The array is special.");
         } else {
